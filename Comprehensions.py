85ca645881e10c741d0a182bf823f720a270e632
koffskeyj <koffskeyj@gmail.com>
2016-05-23 13:59:46 -0400

    Updated

--- Comprehensions.py
+++ Comprehensions.py
@@ -14,10 +14,14 @@ def remove_vowels(b):
 def remove_vowels_comp(b):
     new = b
     vowels = ["a", "e", "i", "o", "u"]
-    new = [new.replace(a, "") for a in b.lower() if a in vowels]
+    new = "".join([a for a in b.lower() if a not in vowels])
 
     return new
 
 print(remove_vowels(sentence))
 print(remove_vowels_comp(sentence))
 
+def water_temp_list():
+    headers = ["ID", "Wave Height", "Wave Period", "Avg Waves Per Second",
+               "Water Temp", "Date"]
+    info = 
